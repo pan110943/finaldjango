6ebe0f0e0afdf0a9b26f73ff9447711727d44fdd
Pan110943 <pan.pornpinetpong@gmail.com>
2020-10-01 18:52:10 +0700

    final project

---
+++ stock/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+class Product (models.Model):
+    product_name = models.CharField(max_length=128)
+    product_deteil = models.TextField()
+    product_barcode =models.CharField(max_length=32)
+    product_qty = models.IntegerField()
+    product_price = models.DecimalField(decimal_places=2, max_digits=7)
+    product_image = models.CharField(max_length=64)
+    product_status = models.IntegerField()
---
+++ stock/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Product
+# from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request,'frontend/index.html', {'products':products})
+
+def about(request):
+    return render(request,'frontend/about.html')
+    
+def contact(request):
+    return render(request,'frontend/contact.html')
